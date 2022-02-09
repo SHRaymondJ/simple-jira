3ff032bfabc9554b1cb6fce9146aff5c0941471b
RaymondJ <396945399@qq.com>
2022-02-09 21:42:05 +0800

    useUndo

--- src/utils/useAsync.ts
+++ src/utils/useAsync.ts
@@ -1,7 +1,7 @@
-import { useCallback, useState } from 'react'
-import { useMountedRef } from 'utils'
+import { useCallback, useReducer, useState } from 'react'
+import { useMountedRef } from 'utils/index'
 
-interface State<D = null> {
+interface State<D> {
     error: Error | null
     data: D | null
     stat: 'idle' | 'loading' | 'error' | 'success'
@@ -9,41 +9,63 @@ interface State<D = null> {
 
 const defaultInitialState: State<null> = {
     stat: 'idle',
-    error: null,
     data: null,
+    error: null,
 }
 
-const defaultThrowOnError = {
+const defaultConfig = {
     throwOnError: false,
+}
+
+const useSafeDispatch = <T>(dispatch: (...args: T[]) => void) => {
+    const mountedRef = useMountedRef()
+    return useCallback(
+        (...args: T[]) => (mountedRef.current ? dispatch(...args) : void 0),
+        [dispatch, mountedRef]
+    )
 }
 
 export const useAsync = <D>(
     initialState?: State<D>,
-    initialThrowOnError?: typeof defaultThrowOnError
+    initialConfig?: typeof defaultConfig
 ) => {
-    const [state, setState] = useState({
-        ...defaultInitialState,
-        ...initialState,
-    })
-    // useState 设置函数的话，需要使用回调，因为useState(()=>{}) 已经被用作 惰性加载
+    const config = { ...defaultConfig, ...initialConfig }
+    const [state, dispatch] = useReducer(
+        (state: State<D>, action: Partial<State<D>>) => ({
+            ...state,
+            ...action,
+        }),
+        {
+            ...defaultInitialState,
+            ...initialState,
+        }
+    )
+    const safeDispatch = useSafeDispatch(dispatch)
+    // useState直接传入函数的含义是：惰性初始化；所以，要用useState保存函数，不能直接传入函数
+    // https://codesandbox.io/s/blissful-water-230u4?file=/src/App.js
     const [retry, setRetry] = useState(() => () => {})
-    const mountedRef = useMountedRef()
-    const setData = useCallback((data: D) => {
-        setState({
-            data,
-            stat: 'success',
-            error: null,
-        })
-    }, [])
 
-    const setError = useCallback((error: Error) => {
-        setState({
-            error,
-            stat: 'error',
-            data: null,
-        })
-    }, [])
+    const setData = useCallback(
+        (data: D) =>
+            safeDispatch({
+                data,
+                stat: 'success',
+                error: null,
+            }),
+        [safeDispatch]
+    )
 
+    const setError = useCallback(
+        (error: Error) =>
+            safeDispatch({
+                error,
+                stat: 'error',
+                data: null,
+            }),
+        [safeDispatch]
+    )
+
+    // run 用来触发异步请求
     const run = useCallback(
         (promise: Promise<D>, runConfig?: { retry: () => Promise<D> }) => {
             if (!promise || !promise.then) {
@@ -54,30 +76,31 @@ export const useAsync = <D>(
                     run(runConfig?.retry(), runConfig)
                 }
             })
-            setState((prevState) => ({ ...prevState, stat: 'loading' }))
+            safeDispatch({ stat: 'loading' })
             return promise
                 .then((data) => {
-                    if (mountedRef.current) setData(data)
+                    setData(data)
                     return data
                 })
                 .catch((error) => {
+                    // catch会消化异常，如果不主动抛出，外面是接收不到异常的
                     setError(error)
-                    if (initialThrowOnError?.throwOnError)
-                        return Promise.reject(error)
+                    if (config.throwOnError) return Promise.reject(error)
                     return error
                 })
         },
-        [initialThrowOnError?.throwOnError, mountedRef, setData, setError]
+        [config.throwOnError, setData, setError, safeDispatch]
     )
 
     return {
+        isIdle: state.stat === 'idle',
         isLoading: state.stat === 'loading',
         isError: state.stat === 'error',
-        isIdle: state.stat === 'idle',
         isSuccess: state.stat === 'success',
+        run,
         setData,
         setError,
-        run,
+        // retry 被调用时重新跑一遍run，让state刷新一遍
         retry,
         ...state,
     }
